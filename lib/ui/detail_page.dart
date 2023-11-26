c7c85cf2b39ddac2fa886b21bf7fc4b33b79906a
Sakuntha <sakunthadayadara2001@gmail.com>
2023-11-26 17:42:14 +0530

    Home Page Done with Api

---
+++ lib/ui/detail_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  final dailyForecastWeather;
+
+  const DetailPage({Key? key, this.dailyForecastWeather}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
